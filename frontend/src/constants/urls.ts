4969b24ddd7cc2f1353437be1bb57858a5cf51a9
MustafaNova <erturkmustafacan639@gmail.com>
2026-05-16 09:19:32 +0200

    add url

--- frontend/src/constants/urls.ts
+++ frontend/src/constants/urls.ts
@@ -1,4 +1,4 @@
-export const SERVER_URL = 'http://localhost:3000'
+export const SERVER_URL = 'https://funcode-g40h.onrender.com'
 
 export const API_URLS = {
     REGISTER: `${SERVER_URL}/auth/register`,
